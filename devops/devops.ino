200907b5fa5a81ac010a13c9ce82bfc9a49631ed
Abi Renhart <arenhart@ALITD010.local>
2017-03-02 13:44:31 -0800

    make prettier

--- devops/devops.ino
+++ devops/devops.ino
@@ -1,6 +1,8 @@
 #include <RGB.h>
 #include <RGBMatrix.h>
 
+char colors[7] = {RED, GREEN, BLUE, ORANGE, MAGENTA, TEAL, WHITE};
+
 void setup()
 {
     delay(5000);
@@ -9,5 +11,10 @@ void setup()
 
 void loop()
 {
-  RGBMatrix.scroll("DEVOPS FOR LYFE", RED, 30);
+  char * message = "INFRA FOR LYFE";
+
+  for(char color=1; color<7; color++)
+  {
+     RGBMatrix.scroll(message, colors[color], 30);
+  }
 }
